e936e319a82921ac0a32718b197c6c744220115f
Kohei KONISHI <konishi2911@icloud.com>
2020-10-26 23:22:59 +0900

    Initial Commit

---
+++ BudgetManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BudgetManager
+//
+//  Created by Kohei Konishi on 2020/10/26.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
